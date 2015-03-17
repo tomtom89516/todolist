19d6fa38c8052078bdb45644e33bf28e3ab1aaf8
tomtom89516 <abcdef830327@gmail.com>
2015-03-17 17:31:44 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,45 @@
+jsonp_handler({
+ "version": "1",
+ "build": 52,
+ "title": "todolist",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/tomtom89516/todolist/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css"
+ ],
+ "filesizes": [
+  547,
+  115,
+  652507,
+  96666,
+  277980,
+  778,
+  35607,
+  113502
+ ],
+ "filedates": [
+  "2015-03-17T06:26:34.470Z",
+  "2015-03-17T07:33:23.925Z",
+  "2015-03-17T06:26:34.527Z",
+  "2015-03-17T09:27:36.848Z",
+  "2015-03-17T06:26:34.490Z",
+  "2015-03-17T09:27:28.274Z",
+  "2015-03-17T06:26:34.467Z",
+  "2015-03-17T06:26:34.459Z"
+ ],
+ "date": "2015-03-17T09:27:37.831Z"
+})
---
+++ src/listview.jsx
@@ -0,0 +1,29 @@
+var React=require("react");
+var Reflux=require("reflux");
+var store=require("./store");
+var actions=require("./actions")
+var Listview = React.createClass({
+	mixins:[Reflux.listenTo(store,"onStore")]
+	,getInitialState:function(){
+		return {data:[]};
+	}
+	,onStore:function(data) {
+		this.setState({data:data});
+	}
+	,toggleDone:function(e) {
+		var itemidx=e.target.dataset.idx;
+		//console.log(itemidx);
+		actions.toggle(itemidx);
+	}
+	,renderItem:function(item,idx) {
+		return <div >
+		<li data-idx={idx} onClick={this.toggleDone}
+			className={item.done?"done":""}>{item.caption}</li></div>
+	}
+	,render: function() {
+		return <ol>
+			{this.state.data.map(this.renderItem)}
+		</ol>;
+	}
+});
+module.exports=Listview
